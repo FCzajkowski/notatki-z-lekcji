eaf110f0ba9bc58660c17f9a749c5cccedf9b2da
Franciszek <franiu325@gmail.com>
2024-10-07 14:50:29 +0200

    Update Zmienne-Java.java

--- Zmienne-Java.java
+++ Zmienne-Java.java
@@ -14,11 +14,39 @@ public class Mmain{
     i pozniej inicjacja: 
     <nazwa> = <wartosc>
     */
-    int b;
-    b = 10;
+    String b;
+    b = " śema";
     System.out.print("a: ");
     System.out.print(a);
     System.out.print("b: ");
     System.out.print(b);
+
+    /* 
+    Typy Danych 
+
+    W Javie istnieje kilka podstawowych typów zmiennych:
+    
+    Liczby całkowite:
+    
+    byte (8-bitowy)
+    short (16-bitowy)
+    int (32-bitowy)
+    long (64-bitowy)
+    Liczby zmiennoprzecinkowe:
+    
+    float (32-bitowy)
+    double (64-bitowy)
+    Znak:
+    
+    char (16-bitowy, przechowuje pojedynczy znak)
+    Wartości logiczne:
+    
+    boolean (przechowuje wartość true lub false)
+    Łańcuchy znaków:
+    
+    String (klasa, przechowuje tekst)
+    Przykłady:
+    
+    */
   }
 }
